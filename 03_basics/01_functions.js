f6cfbc57adcbd4b63a78e8198066e777d9e732e7
Pradyuman Sarswat <pradyumansarswat@gmail.com>
2024-09-20 17:21:53 +0530

    some things about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,9 +31,9 @@
 
 // third approach to write function
 
-function addTwoNumers(number1, number2) {
-   return number1 + number2
-}
+// function addTwoNumers(number1, number2) {
+//     return number1 + number2
+// }
 
 
 // addTwoNumers(3, "4") // 34
@@ -42,22 +42,64 @@ function addTwoNumers(number1, number2) {
 
 // addTwoNumers(3, null)  //3
 
-const result = addTwoNumers(3, 8)
+// const result = addTwoNumers(3, 8)
 
 // console.log("result: ", result)
 
 
 
 
-function loginUserMessage (username = "acp"){
-    if(!username){
-        console.log("please enter a username")
-        return
-    }
- return `${username} just loggedIn`
-}
+// function loginUserMessage(username = "acp") {
+//     if (!username) {
+//         console.log("please enter a username")
+//         return
+//     }
+//     return `${username} just loggedIn`
+// }
 
 // console.log(loginUserMessage("Pradyuman"))
 
-console.log(loginUserMessage("Pradyuman"))
+// console.log(loginUserMessage("Pradyuman"))
 
+
+
+
+
+
+function calculateCartPrice(val1, val2, ...num1) {
+
+    return num1
+}
+
+// console.log(calculateCartPrice(2))  // 2
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+
+const user = {
+    username: "Pradyuman",
+    price: 199
+}
+
+function handleobject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// console.log(handleobject(user))
+
+// if we change price to prices or think of ant typo error then clg will show undefined for that typo error 
+
+// handleobject({
+//     username: "ACP",
+//     price: 399
+// })
+
+// for arrays
+
+const myNewArray = [200, 400, 600]
+
+function returnSecondValue(getarray) {
+return getarray[1]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 1000]))
